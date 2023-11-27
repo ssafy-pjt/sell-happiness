aa6454c2aa67d9faa1c2a8b1417dfe70c02d1a26
minjun <hitobi1014@gmail.com>
2023-11-27 22:26:12 +0900

    init

---
+++ sellhappiness/src/main/java/com/sfs/sellhappiness/SellhappinessApplication.java
@@ -0,0 +1,13 @@
+package com.sfs.sellhappiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellhappinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellhappinessApplication.class, args);
+	}
+
+}
