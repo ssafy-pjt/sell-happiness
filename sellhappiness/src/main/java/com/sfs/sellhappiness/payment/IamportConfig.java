f2621bf5271d30c77d6e0a7237140ceb581cf73e
donghun <huni19541@gmail.com>
2023-12-14 10:53:39 +0900

    yml 읽어오는 코드 예시

---
+++ sellhappiness/src/main/java/com/sfs/sellhappiness/payment/IamportConfig.java
@@ -0,0 +1,21 @@
+package com.sfs.sellhappiness.payment;
+
+import com.sfs.sellhappiness.global.config.YamlLoadFactory;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "iamport")
+@PropertySource(value = {"application-iamport.yml"}, factory = YamlLoadFactory.class)
+@Getter
+public class IamportConfig {
+
+    @Value("${iamport.api.key}")
+    private String key;
+
+    @Value("${iamport.api.secret}")
+    private String secret;
+}
