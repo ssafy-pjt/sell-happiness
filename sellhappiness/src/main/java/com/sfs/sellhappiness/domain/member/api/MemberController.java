d23c59091bb39555ba377f5652b671f275fe9b17
minjun <hitobi1014@gmail.com>
2023-12-14 20:48:20 +0900

    JWT Util 임시커밋2

--- sellhappiness/src/main/java/com/sfs/sellhappiness/domain/member/api/MemberController.java
+++ sellhappiness/src/main/java/com/sfs/sellhappiness/domain/member/api/MemberController.java
@@ -1,7 +1,9 @@
 package com.sfs.sellhappiness.domain.member.api;
 
 import com.sfs.sellhappiness.domain.member.application.MemberService;
+import com.sfs.sellhappiness.global.auth.JwtProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
+    private final JwtProperties properties;
 
     // jwt 테스트
     // TODO: 회원 DTO 만들어서 JWT 테스트
     @PostMapping("/login")
     public ResponseEntity<String> login() {
+        log.info("■■■■ 컨트롤러 ");
+        log.info("시크릿키", properties.getSecret());
+        log.info("url", properties.getUrl());
+        log.info("■■■■ 컨트롤러 종료");
+
         return ResponseEntity.ok()
                 .body(memberService.login());
     }
+
+
 }
--- sellhappiness/src/main/java/com/sfs/sellhappiness/domain/member/application/MemberServiceImpl.java
+++ sellhappiness/src/main/java/com/sfs/sellhappiness/domain/member/application/MemberServiceImpl.java
@@ -1,13 +1,17 @@
 package com.sfs.sellhappiness.domain.member.application;
 
 import com.sfs.sellhappiness.global.auth.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService{
+
+    private final JwtUtil jwtUtil;
 
     @Override
     public String login() {
-        return JwtUtil.createJWT();
+        return jwtUtil.createJWT();
     }
 }
